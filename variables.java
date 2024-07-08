f59ee390ca5a714fe09f6e7781bbb5f079b33d8d
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-08 10:49:12 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+  public static void main(String args[]) {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    System.out.println(a);
+    System.out.println(b);
+
+  #to add the 2 given numbers----------
+    System.out.println("Add the given numbers :" +sum);
