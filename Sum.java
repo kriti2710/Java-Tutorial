24d5878d3aeb85506dbdc90730b4d60f89546e19
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-09 23:43:28 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+// Adding 2 number
+
+public class Sum {
+  public static void main(String args[]) {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    System.out.println("Sum of a + b : " + sum);
+
+
+  // output
+  //-----Sum of a + b : 15 
