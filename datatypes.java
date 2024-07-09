527ad373768e94abe6b1b2ce8626db7ad9e9e742
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-09 23:39:18 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,20 @@
+//datatypes in java
+/*primitive datatype:
+bytes, short, char, boolean, int, long, float, double
+
+Non-primitives datatypes:
+string, array, class, object, interfaces
+
+java is a typed language.......*/
+public class dataypes {
+  public static void main(String args[]) {
+boolean b = 8;
+System.out.println(b);
+char ch = 'ch';
+System.out.println(ch);
+// boolean var="True";
+// System.out.println(var);
+// int a=10;
+// float price=10.5;
+//short n=420;
+
