e4c9fbe86a7e86f503ecc9654b25d8091ccb334d
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-10 23:38:35 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,22 @@
+# to write input values in various way
+import java.util.*;
+public class Input{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // Double number = sc.nextDouble();
+        // System.out.println(number);
+
+        boolean value = sc.nextBoolean();
+        System.out.println(value);
