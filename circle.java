cb9f726bd3c03e5cd9c45e11c0cebf30caa56a2d
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-11 23:01:42 +0530

    Update circle.java

--- circle.java
+++ circle.java
@@ -3,6 +3,7 @@
 public class circle {
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
+    System.out.println("radius of circle : ");
     float radius = sc.nextInt();
     float area = 3.14f * radius * radius ;
     System.out println("area of circle is :" +area);
