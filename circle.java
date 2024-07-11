4635030c278fed54e277058238a316f1690314ca
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-11 22:59:19 +0530

    Create circle.java

---
+++ circle.java
@@ -0,0 +1,10 @@
+# find the area of circle -----------
+
+public class circle {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    float radius = sc.nextInt();
+    float area = 3.14f * radius * radius ;
+    System.out println("area of circle is :" +area);
+  }
+}
