efb91aa2e0c75d09fa3c52081ef3c093f57c2b92
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-13 21:32:43 +0530

    Create typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class typecasting {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    float a = 25.16f;
+    int b =(int)a;
+    System.out.println(b);
+
+  char ch = 'a';
+    int number = ch;
+    System.out.println(number);
