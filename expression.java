389a0c21e84bfbe7b7ab493d7e2087b5c2fef31e
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-15 23:25:30 +0530

    Create expression.java

---
+++ expression.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class expression {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    char a = 'a';
+    char b = 'b';
+    System.out.println((int)(b));
+    Syste..out.println((int)(a));
+    System.out.print(b-a);
+
+  # ------output: ---------
+  #  98
+  #  97
+  #   1
