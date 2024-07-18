a40a43db3cc92f148297f9c81952b05a95f88156
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-18 23:25:55 +0530

    operators.java

---
+++ operators.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class oprators {
+    public static void main(String args[]) {
+       
+     arithmetic oprator------------>>>>binary oprator  
+        int a = 10;
+        int b = 5;
+        System.out.println("add = " + (a+b));
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
