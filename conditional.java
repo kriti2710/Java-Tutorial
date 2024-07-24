e3032d4d96fb6562ab751a944a883c70ca4e67a3
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-25 00:33:42 +0530

    Update conditional.java

--- conditional.java
+++ conditional.java
@@ -3,32 +3,43 @@ import java.util.*;
 public class conditional {
     public static void main(String args[]) {
         
-        if else----------------
-        int age = 16;
-        if (age >= 18) {
-            System.out.println("vote");
-        }
-        else if (age > 13 && age <18) {
-            System.out.println("teenager");
+        // if else----------------
+        // int age = 16;
+        // if (age >= 18) {
+        //     System.out.println("vote");
+        // }
+        // else if (age > 13 && age <18) {
+        //     System.out.println("teenager");
+        // }
+        // else {
+        //     System.out.println("not eligible for vote");
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // int time = sc.nextInt();
+
+        // if (time <= 30) {
+        //     System.out.println("Move straight");
+        // }
+
+        // else if (time > 30 && time <= 60) {
+        //     System.out.println("move left");
+        // }
+        // else if (time > 60 && time <= 90){
+        //     System.out.println("move right");
+        // }
+
+        // else {
+        //     System.out.println("STOP");
+        // }
+
+    PRINT THE LARGEST OF 2 NUMBER---------------
+        int a = 2;
+        int b = 5;
+
+        if (a >= b) {
+            System.out.println("a is largest of 2");
         }
         else {
-            System.out.println("not eligible for vote");
-        }
-
-        Scanner sc = new Scanner(System.in);
-        int time = sc.nextInt();
-
-        if (time <= 30) {
-            System.out.println("Move straight");
-        }
-
-        else if (time > 30 && time <= 60) {
-            System.out.println("move left");
-        }
-        else if (time > 60 && time <= 90){
-            System.out.println("move right");
-        }
-
-        else {
-            System.out.println("STOP");
+            System.out.println("b is largest of 2 ");
         }
