192590c1d520f15832e4e3b3ec49fa1e2fdef90b
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-07-26 23:40:52 +0530

    Update conditional.java

--- conditional.java
+++ conditional.java
@@ -33,13 +33,26 @@ public class conditional {
         //     System.out.println("STOP");
         // }
 
-    PRINT THE LARGEST OF 2 NUMBER---------------
-        int a = 2;
-        int b = 5;
+    // PRINT THE LARGEST OF 2 NUMBER---------------
+    //     int a = 2;
+    //     int b = 5;
 
-        if (a >= b) {
-            System.out.println("a is largest of 2");
-        }
-        else {
-            System.out.println("b is largest of 2 ");
-        }
+    //     if (a >= b) {
+    //         System.out.println("a is largest of 2");
+    //     }
+    //     else {
+    //         System.out.println("b is largest of 2 ");
+    //     }
+
+         PRINT IF A NUMBER IS ODD OR EVEN-----------
+            Scanner sc = new Scanner(System.in);
+            int number = sc.nextInt();
+
+            if (number % 2 == 0) {
+                System.out.println("EVEN");
+            }
+            else {
+                System.out.println("ODD");
+            }
+ 
+
